e4a9dcbd84a39bdd2034ac97770b5014e51d7f41
matan211 <95849253+matan211@users.noreply.github.com>
2024-09-19 18:53:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from utils import read_video, save_video
 from trackers.tracker import Tracker
+import cv2
+from team_assigner import TeamAssigner
+from player_ball_assigner.PlayerBallAssigner import PlayerBallAssigner
 
 def main():
     # Read video
@@ -11,7 +14,43 @@ def main():
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
                                        stub_path="stubs/track_stubs.pkl")
+    # # Save cropped image of a player
+    # for track_id, player in tracks['players'][0].items():
+    #     bbox = player['bbox']
+    #     frame = video_frames[0]
 
+    #     # crop bbox grom frame
+    #     cropped_image = frame[int(bbox[1]) : int(bbox[3]), int(bbox[0]) : int(bbox[2])]
+
+    #     # Save the cropped image
+    #     cv2.imwrite(f'output_videos/cropped_img.jpg', cropped_image)
+    #     break
+
+    # Interpolate Ball Positions
+    tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
+
+    # Assign player Teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], 
+                                    tracks['players'][0])
+    
+    for frame_num, player_track in enumerate(tracks['players']):
+            for player_id, track in player_track.items():
+                 team = team_assigner.get_player_team(video_frames[frame_num],
+                                                      track['bbox'],
+                                                      player_id)
+                 tracks['players'][frame_num][player_id]['team'] = team
+                 tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+
+    # Assign Ball Aquisition
+    player_assigner = PlayerBallAssigner()
+    for frame_num, player_track in enumerate(tracks['players']):
+         ball_box = tracks['ball'][frame_num][1]['bbox']
+         assigned_player = player_assigner.assign_ball_to_player(player_track, ball_box)
+
+         if assigned_player != -1:
+              tracks['players'][frame_num][assigned_player]['has_ball'] = True
+    
     # Draw output
     ## Draw object Tracks
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
