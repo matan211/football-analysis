da28daf867d5451ff4ef0fe5b3765ad0ab8a42b5
matan211 <95849253+matan211@users.noreply.github.com>
2024-10-14 11:35:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import numpy as np
 from team_assigner import TeamAssigner
 from player_ball_assigner.PlayerBallAssigner import PlayerBallAssigner
 from camera_movement_estimator import CameraMovementEstimator
+from view_transformer import ViewTransformer
+from speed_ans_distance_estimator import SpeedAndDistance_Estimator
 
 def main():
     # Read video
@@ -38,9 +40,17 @@ def main():
                                                                               stub_path='stubs/camera_movement_stub.pkl')
     camera_movement_estimator.add_adjust_positions_to_tracks(tracks, camera_movement_per_frame)
     
+    # View Transformer
+    view_transformer = ViewTransformer()
+    view_transformer.add_transformed_position_to_tracks(tracks)
+    
     # Interpolate Ball Positions
     tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
 
+    # Speed and distance estimator
+    speed_and_distance_estimator = SpeedAndDistance_Estimator()
+    speed_and_distance_estimator.add_speed_and_distance_to_tracks(tracks)
+    
     # Assign player Teams
     team_assigner = TeamAssigner()
     team_assigner.assign_team_color(video_frames[0], 
@@ -75,6 +85,10 @@ def main():
 
     ## Draw Camera Movement
     output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames, camera_movement_per_frame)
+    
+    ## Draw Speed and Distance
+    speed_and_distance_estimator.draw_speed_and_distance(output_video_frames, tracks)
+    
     # Save video
     save_video(output_video_frames, 'output_videos/output_video.avi')
 
